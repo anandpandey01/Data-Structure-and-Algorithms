f5fcd4e373f5f516c0de706e74810e712f4e1dbc
Anand Pandey <91401035+anandpandey01@users.noreply.github.com>
2022-06-27 15:38:15 +0530

    Added solution - LeetHub

---
+++ 9. Addition Under Modulo - GFG/9.-addition-under-modulo.java
@@ -0,0 +1,41 @@
+// { Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+class GFG
+{
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        
+        //taking testcases
+        int t = Integer.parseInt(read.readLine());
+        
+        while(t-- > 0) {
+            String[] str = read.readLine().trim().split(" ");
+            
+            //taking input a and b
+            Long a = Long.parseLong(str[0]);
+            Long b = Long.parseLong(str[1]);
+            
+            //calling method sumUnderModulo()
+            System.out.println(new Solution().sumUnderModulo(a,b));
+        }
+    }
+}// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution {
+    public static long sumUnderModulo(long a, long b){
+        // code here
+        long r = (long)((Math.pow(10,9)+7));
+        
+        return (a%r+b%r)%r;
+        
+    }   
+}
