f4e741ecafecdf1ba54605a3ba68f6bc8ddb85cf
Anand Pandey <91401035+anandpandey01@users.noreply.github.com>
2022-07-27 16:58:23 +0530

    Time: 0 ms (100.00%), Space: 40.1 MB (85.10%) - LeetHub

--- 1512-number-of-good-pairs/1512-number-of-good-pairs.java
+++ 1512-number-of-good-pairs/1512-number-of-good-pairs.java
@@ -1,14 +1,10 @@
 class Solution {
     public int numIdenticalPairs(int[] nums) {
         
-        
+        int []res=new int[101];
         int count=0;
         for(int i=0;i<nums.length;i++){
-            for(int j=i+1;j<nums.length;j++){
-                if(nums[i]==nums[j]){
-                    count++;
-                }
-            }
+            count=count+res[nums[i]]++;
         }
         return count;
     }
