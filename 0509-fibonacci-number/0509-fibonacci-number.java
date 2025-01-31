7537fdb16d61911395c7dd32670591d96dff0747
Anand Pandey <91401035+anandpandey01@users.noreply.github.com>
2025-01-31 10:45:50 +0530

    Time: 0 ms (100%), Space: 40.5 MB (38.19%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,8 +1,15 @@
 class Solution {
     public int fib(int n) {
-        if(n==0) return 0;
-        if(n==1) return 1;
-        return fib(n-1) + fib(n-2);
-        
+       int prev = 0, curr =1;
+       int sum = 0;
+       if(n==1) return 1;
+        while(n>1){
+            sum = prev+ curr;
+            prev = curr;
+            curr = sum;
+            n--;
+
+        }
+        return sum;
     }
 }
