8b979f6981c05699b27bd2c98329eaee93d2d172
Anand Pandey <91401035+anandpandey01@users.noreply.github.com>
2025-02-02 15:44:26 +0530

    Time: 2 ms (84.83%), Space: 45.8 MB (79.56%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -3,9 +3,11 @@ class Solution {
         int n=arr.length,i=0;
         for(int j=0;j<n;j++){
             if(arr[j]!=0){
-                int temp = arr[i];
+                if(i!=j){
+                     int temp = arr[i];
                 arr[i] = arr[j];
                 arr[j] = temp;
+                }   
                 i++;
             }
         }
