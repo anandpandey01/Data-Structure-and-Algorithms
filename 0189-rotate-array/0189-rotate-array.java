e7aa983aeea102a73f1964bd14775e62366e2b7a
Anand Pandey <91401035+anandpandey01@users.noreply.github.com>
2025-02-02 12:13:11 +0530

    Time: 2 ms (30.09%), Space: 57.6 MB (21.65%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.java
+++ 0189-rotate-array/0189-rotate-array.java
@@ -3,21 +3,21 @@ class Solution {
         int n = nums.length;
         k = k%n;
         int left=0, right=n-1;
-        while(left<=right){
+        while(left<right){
             int temp=nums[left];
             nums[left] = nums[right];
             nums[right] = temp;
             left++; right--;
         }
         left =0 ; right = (k-1);
-          while(left<=right){
+          while(left<right){
             int temp=nums[left];
             nums[left] = nums[right];
             nums[right] = temp;
             left++; right--;
         }
          left =k ; right = (n-1);
-          while(left<=right){
+          while(left<right){
             int temp=nums[left];
             nums[left] = nums[right];
             nums[right] = temp;
