60e3e9b72e6c487a0775eda6619ed3310a55fc61
Anand Pandey <91401035+anandpandey01@users.noreply.github.com>
2025-02-03 15:53:46 +0530

    Time: 6 ms (31.37%), Space: 46.1 MB (70.82%) - LeetHub

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -1,9 +1,16 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        int res=0;
-        for(int i=0;i<nums.length;i++){
-            res=res^nums[i];
+        Arrays.sort(nums);
+        int pointer = nums.length-1,i=nums.length-2;
+        while(pointer>0){
+                if(nums[i]== nums[pointer]){
+                    pointer-=2;
+                    i-=2;
+                }
+                else{
+                    return nums[pointer];
+                }
         }
-        return res;
+        return nums[0];
     }
 }
