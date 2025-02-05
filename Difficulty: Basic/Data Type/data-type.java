ebec259e395e8ff89c76df3fbf49e06aa691bd77
Anand Pandey <91401035+anandpandey01@users.noreply.github.com>
2025-02-05 11:24:03 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Data Type/data-type.java
@@ -0,0 +1,46 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.lang.*;
+import java.math.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        while (T-- > 0) {
+            sc.nextLine();
+            String s = sc.next();
+            Solution obj = new Solution();
+            int ans = obj.dataTypeSize(s);
+            String Character = "Character";
+            if (Character.equals(s) && ans == 2) {
+                ans = 1;
+            }
+            System.out.println(ans);
+        
+System.out.println("~");
+}
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    static int dataTypeSize(String str) {
+        // code here
+         HashMap<String,Integer> hs = new HashMap();
+        hs.put("Character",2);
+        hs.put("Integer",4);
+        hs.put("Long",8);
+        hs.put("Float",4);
+        hs.put("Double",8);
+        
+        return hs.getOrDefault(str,-1);
+    }
+}
