04e99b2e64b92c980820ae3b6bbfbbee491ffcd3
Anand Pandey <91401035+anandpandey01@users.noreply.github.com>
2025-02-20 11:01:18 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Find nth root of m/find-nth-root-of-m.java
@@ -0,0 +1,40 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine().trim());
+        while (T-- > 0) {
+            int n = Integer.parseInt(br.readLine().trim());
+            int m = Integer.parseInt(br.readLine().trim());
+            Solution ob = new Solution();
+            int ans = ob.nthRoot(n, m);
+            System.out.println(ans);
+
+            System.out.println("~");
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    public int nthRoot(int n, int m) {
+        // code here
+         double ans = Math.pow(m,(1.0/n));
+         int rounded = (int) Math.round(ans);  // Round to nearest integer
+        // Verify if the rounded value is a valid nth root
+        if (Math.pow(rounded, n) == m) {
+            return rounded;
+        }
+         return -1;
+    }
+}
