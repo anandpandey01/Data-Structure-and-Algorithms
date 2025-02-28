6ef5fccbcf4a196fef406a669bc8ad5ae7d70421
Anand Pandey <91401035+anandpandey01@users.noreply.github.com>
2025-02-28 15:59:10 +0530

    Time: 3 ms (91.06%), Space: 44.4 MB (31.92%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.java
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -1,23 +1,32 @@
+import java.util.*;
+
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
-        int[] ans = new int[nums1.length];
+        Map<Integer, Integer> nextGreaterMap = new HashMap<>();
+        Stack<Integer> stack = new Stack<>();
+        
+        // Traverse nums2 from RIGHT to LEFT
+        for (int i = nums2.length - 1; i >= 0; i--) {
+            int num = nums2[i];
 
-        for (int i = 0; i < nums1.length; i++) {
-            boolean found = false; // Track if we found nums1[i] in nums2
-            ans[i] = -1; // Default to -1 in case no greater element is found
-
-            for (int j = 0; j < nums2.length; j++) {
-                if (nums1[i] == nums2[j]) {
-                    found = true;
-                }
-
-                // Once we have found nums1[i], start looking for the next greater element
-                if (found && nums2[j] > nums1[i]) {
-                    ans[i] = nums2[j];
-                    break;
-                }
+            // Maintain a decreasing stack
+            while (!stack.isEmpty() && stack.peek() <= num) {
+                stack.pop();
             }
+
+            // If stack is empty, no greater element; otherwise, stack's top is NGE
+            nextGreaterMap.put(num, stack.isEmpty() ? -1 : stack.peek());
+
+            // Push the current element to stack
+            stack.push(num);
         }
-        return ans;
+        
+        // Build the result array for nums1 using the hash map
+        int[] result = new int[nums1.length];
+        for (int i = 0; i < nums1.length; i++) {
+            result[i] = nextGreaterMap.get(nums1[i]);
+        }
+        
+        return result;
     }
 }
