ea5a8310c3bfdc7fb7751f28fd281eca9e43a27b
Anand Pandey <91401035+anandpandey01@users.noreply.github.com>
2025-04-24 16:49:10 +0530

    Time: 1 ms (62.65%), Space: 46.2 MB (74.31%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -1,20 +1,20 @@
 class Solution {
     public int trap(int[] arr) {
         int n = arr.length;
-        int lmax[] = new int[n]; 
-        int rmax[] = new int[n]; 
-        lmax[0]=  arr[0]; rmax[n-1]=  arr[n-1]; 
-        for(int i=1; i<n; i++){
-            lmax[i] = Math.max(lmax[i-1],arr[i]);
-        }
-        for(int i=n-2; i>=0; i--){
-            rmax[i] = Math.max(rmax[i+1],arr[i]);
-        }
-        int res =0;
-        for(int k=0; k<n; k++){
-             res = res+ Math.min(lmax[k],rmax[k])-arr[k];
-        }
-        return res;
+        int[] lmax = new int[n];
+        lmax[0] = arr[0];
+        for (int i = 1; i < n; i++) 
+            lmax[i] = Math.max(lmax[i - 1], arr[i]);
         
+        int[] rmax = new int[n];
+        rmax[n - 1] = arr[n - 1];
+        for (int i = n - 2; i >= 0; i--) 
+            rmax[i] = Math.max(rmax[i + 1], arr[i]);
+
+        int ans = 0;
+        for (int i = 0; i < n; i++) 
+            ans += Math.min(lmax[i], rmax[i]) - arr[i];
+
+        return ans;
     }
 }
