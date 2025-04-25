383a330952acc20e371d48e02931a1fcff7f03df
Anand Pandey <91401035+anandpandey01@users.noreply.github.com>
2025-04-25 19:04:15 +0530

    Time: 4 ms (94.89%), Space: 45.3 MB (86.42%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.java
+++ 0735-asteroid-collision/0735-asteroid-collision.java
@@ -1,31 +1,37 @@
 class Solution {
     public int[] asteroidCollision(int[] arr) {
         Stack<Integer> st = new Stack<>();
-        for (int a : arr) {
-        boolean destroyed = false;
-
-        while (!st.isEmpty() && a < 0 && st.peek() > 0) {
-            if (st.peek() < -a) {
-                st.pop();
-                continue;
-            } else if (st.peek() == -a) {
-                st.pop();
+        int n = arr.length;
+        for (int i = 0; i < n; i++) {
+            if (arr[i] > 0 || st.isEmpty()) {
+                st.push(arr[i]);
+            } else {
+                while (!st.isEmpty()) {
+                    int top = st.peek();
+                    int abs = Math.abs(arr[i]);
+                    if(top<0){
+                        st.push(arr[i]);
+                        break;
+                    }
+                    if (abs > top) {
+                        st.pop();
+                        if (st.isEmpty()) {
+                            st.push(arr[i]);
+                            break;
+                        }
+                    } else if (abs == top) {
+                        st.pop();
+                        break;
+                    } else if (abs < top) {
+                        break;
+                    }
+                }
             }
-            destroyed = true;
-            break;
         }
-
-        if (!destroyed) {
-            st.push(a);
+        int []res =  new int[st.size()];
+        for(int i=st.size()-1;i>=0;i--){
+            res[i] = st.get(i);
         }
-    }
-
-    // Copy from bottom to top
-    int[] res = new int[st.size()];
-    for (int i = 0; i < st.size(); i++) {
-        res[i] = st.get(i);
-    }
-
-    return res;
+        return res;
     }
 }
