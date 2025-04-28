0217d21cc040dd6957d7288f45044b46d747db5f
Anand Pandey <91401035+anandpandey01@users.noreply.github.com>
2025-04-28 09:41:28 +0530

    Time: 2 ms (97.39%), Space: 41.5 MB (92.17%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,23 +1,26 @@
 class Solution {
     public boolean isValid(String s) {
-        Stack<Character> st = new Stack<>();
+        Stack<Character> st =  new Stack<>();
         for(char c : s.toCharArray()){
-            if((c=='(') || (c=='{') || (c=='(')){
-                st.push(c);
-            }
+            if((c == '{') || (c == '[') ||(c == '(')) st.push(c);
             else{
-                while(!st.isEmpty() && matching(st.peek(),c)){
+                if(!st.isEmpty() && isMatching(st.peek(),c)){
                     st.pop();
+                }
+                else{
+                    return false;
                 }
             }
         }
         return (st.isEmpty());
-        
     }
-    boolean matching(char a, char b){
-        return((a=='(')&& b==')')
-                || ((a=='{')&& b=='}')
-                || ((a=='[')&& b==']');
+    public boolean isMatching(char a , char b){
+                if((a=='(' && b == ')')  || (a=='{' && b == '}')  ||
+                 (a=='[' && b == ']')) {
+                    return true;
+                 }  
+                 else{
+                    return false;
+                 }
     }
 }
-    
