22cb73dafc8ab340fb0b5fdfca47ddbc6ef522b7
Anand Pandey <91401035+anandpandey01@users.noreply.github.com>
2025-05-26 18:06:24 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Unique Binary Tree Requirements/unique-binary-tree-requirements.java
@@ -0,0 +1,11 @@
+// User function Template for Java
+
+class Solution {
+
+    public static boolean isPossible(int a, int b) {
+        // Code here
+        if(a==2 && b==2) return false;
+        if(a==2 || b==2) return true;
+         return false;
+    }
+}
