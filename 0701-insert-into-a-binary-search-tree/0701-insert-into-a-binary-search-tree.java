b016082be9fef90e9521e18cb3e73de0d17ae9f3
Anand Pandey <91401035+anandpandey01@users.noreply.github.com>
2025-05-29 12:57:52 +0530

    Time: 0 ms (100%), Space: 45 MB (92.42%) - LeetHub

--- 0701-insert-into-a-binary-search-tree/0701-insert-into-a-binary-search-tree.java
+++ 0701-insert-into-a-binary-search-tree/0701-insert-into-a-binary-search-tree.java
@@ -15,12 +15,26 @@
  */
 class Solution {
     public TreeNode insertIntoBST(TreeNode root, int val) {
-        if(root==null) return new TreeNode(val);
-        if(root.val > val){
-            root.left =  insertIntoBST(root.left,val);
+        TreeNode temp = new TreeNode(val);
+        TreeNode curr = root, parent = null;
+        while(curr!=null){
+            parent = curr;
+            if(curr.val> val){
+                curr = curr.left;                
+            }
+            else if(curr.val< val){
+                curr = curr.right;
+            } 
+            else{
+                return root;
+            }
+        }
+        if(parent == null) return temp;
+        if(parent.val > val){
+            parent.left = temp;
         }
         else{
-            root.right = insertIntoBST(root.right,val);
+            parent.right = temp;
         }
         return root;
     }
