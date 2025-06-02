e9fc98b09aeed581dd18ce81866ac1033449423d
Anand Pandey <91401035+anandpandey01@users.noreply.github.com>
2025-06-02 08:02:41 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Number of Provinces/number-of-provinces.java
+++ Difficulty: Medium/Number of Provinces/number-of-provinces.java
@@ -1,31 +1,24 @@
 // User function Template for Java
 
-class Solution {
+class Solution {               // DFS
     static int numProvinces(ArrayList<ArrayList<Integer>> adj, int V) {
         // code here
-        boolean visited[] = new boolean[adj.size()];
+        boolean visited[] = new boolean[V];
         int count=0;
-        for(int i=0;i<V;i++){
+        for(int i=0; i<V; i++){
             if(visited[i] == false){
-                numProvinces_helper(adj,visited,i);
+                dfs(adj,V,visited,i);
                 count++;
             }
         }
         return count;
     }
-    static void numProvinces_helper(ArrayList<ArrayList<Integer>> adj, boolean visited[],int s) {
-        Queue<Integer> queue = new LinkedList<>();
-        queue.add(s);
-        visited[s] = true;
-        while(!queue.isEmpty()){
-            int u = queue.poll();
-            for (int v = 0; v < adj.get(u).size(); v++) {
-                if(visited[v]==false && adj.get(u).get(v) == 1){
-                    visited[v] = true;
-                    queue.add(v);
-                }
+    static void dfs(ArrayList<ArrayList<Integer>> adj,int V, boolean visited[],int u) {
+        for(int v=0; v<V; v++){
+            if(visited[v]==false && adj.get(u).get(v)==1){
+                visited[v] = true;
+                dfs(adj,V,visited,v);
             }
         }
     }
-   
 };
