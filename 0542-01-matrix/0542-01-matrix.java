0cf80bc6878796f8171e7e7034cdec00085d11ea
Anand Pandey <91401035+anandpandey01@users.noreply.github.com>
2025-06-04 08:37:43 +0530

    Time: 12 ms (92.03%), Space: 47.3 MB (65.58%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.java
+++ 0542-01-matrix/0542-01-matrix.java
@@ -1,42 +1,37 @@
-class Solution {
+class Solution {   // Use a BFS and will traverse from 0 not 1
     int rows;
     int cols;
     public int[][] updateMatrix(int[][] mat) {
         rows = mat.length;
         cols = mat[0].length;
-        int[][] dist = new int[rows][cols];
-        boolean[][] visited = new boolean[rows][cols];
+        int [][] dist = new int[rows][cols];
         Queue<int[]> queue = new LinkedList<>();
-
-        // Enqueue all 0s and mark as visited
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < cols; j++) {
-                if (mat[i][j] == 0) {
-                    queue.offer(new int[]{i, j});
-                    visited[i][j] = true;
+        for(int i=0; i<rows; i++){
+            for(int j=0; j<cols; j++){
+                if(mat[i][j]==0){
+                    dist[i][j]=0;
+                    queue.offer(new int[]{i,j});
+                }
+                else{
+                    dist[i][j]=-1;
                 }
             }
         }
-        int[][] adjList = {{-1, 0}, {0, 1}, {1, 0}, {0, -1}};
-
-        while (!queue.isEmpty()) {
+        int adjList[][] = {{1,0},{-1,0},{0,1},{0,-1}};
+        while(!queue.isEmpty()){
             int[] cell = queue.poll();
-            int r = cell[0];
-            int c = cell[1];
-
-            for (int[] dir : adjList) {
-                int nr = r + dir[0];
-                int nc = c + dir[1];
-
-                if (nr < 0 || nr >= rows || nc < 0 || nc >= cols) continue;
-                if (visited[nr][nc]) continue;
-
-                dist[nr][nc] = dist[r][c] + 1;
-                visited[nr][nc] = true;
-                queue.offer(new int[]{nr, nc});
+            int row = cell[0];
+            int col = cell[1];
+            for(int[] neigbour : adjList){
+                int r = neigbour[0]+row;
+                int c = neigbour[1]+col;
+                if(r<0 || r>=rows || c<0 || c>=cols) continue;
+                if(dist[r][c]!=-1) continue;
+                dist[r][c] = dist[row][col]+1;
+                queue.offer(new int[]{r,c});               
             }
         }
-
         return dist;
+
     }
 }
