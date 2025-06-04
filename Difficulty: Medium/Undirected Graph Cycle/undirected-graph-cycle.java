ef9cdf9650bcf8fd7422a64cca089f94e2b32ff2
Anand Pandey <91401035+anandpandey01@users.noreply.github.com>
2025-06-04 08:05:09 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Undirected Graph Cycle/undirected-graph-cycle.java
+++ Difficulty: Medium/Undirected Graph Cycle/undirected-graph-cycle.java
@@ -1,38 +1,33 @@
 class Solution {
     public boolean isCycle(int V, int[][] edges) {
         // Code here
-        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
-        boolean visited [] = new boolean[V];
+        ArrayList<ArrayList<Integer>> adjList = new ArrayList<>();
         for(int i=0; i<V; i++){
-            adj.add(new ArrayList<>());
+            adjList.add(new ArrayList<>());
         }
-        for(int [] edge : edges){
+        for(int[]edge : edges){
             int u = edge[0];
             int v = edge[1];
-            adj.get(u).add(v);
-            adj.get(v).add(u);
+            adjList.get(u).add(v);
+            adjList.get(v).add(u);
         }
+        boolean visited[] = new boolean[V];
         for(int i=0; i<V; i++){
             if(visited[i]== false){
-                if(dfs(adj,visited,i,-1) == true) return true;
+                if(dfs(V,adjList,visited,i,-1) == true) return true;
             }
         }
         return false;
+        
     }
-    
-    public boolean dfs(ArrayList<ArrayList<Integer>> adj, boolean [] visited,int u,int parent){
-        visited[u] = true;
-        for(int v: adj.get(u)){
+    public boolean dfs(int V, ArrayList<ArrayList<Integer>> adjList,boolean visited[],int s, int parent){
+        visited[s]=true;
+        for(int v: adjList.get(s)){
             if(visited[v]==false){
-                if(dfs(adj,visited,v,u) == true){
-                    return true;
-                }
+               if(dfs(V,adjList,visited,v,s) == true) return true; 
             }
-            else if(v != parent){
-                    return true;
-            }
+            else if(parent != v) return true;
         }
         return false;
+        }
     }
-    
-}
