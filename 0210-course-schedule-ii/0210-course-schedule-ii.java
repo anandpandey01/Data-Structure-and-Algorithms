5a54e10c7f3165b0cde0f9dc0a8b5046f3df3a6b
Anand Pandey <91401035+anandpandey01@users.noreply.github.com>
2025-06-05 13:54:51 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0210-course-schedule-ii/0210-course-schedule-ii.java
@@ -0,0 +1,42 @@
+class Solution {
+    public int[] findOrder(int numCourses, int[][] prerequisites) {
+        ArrayList<ArrayList<Integer>> adjList = new ArrayList<>();
+        int V = numCourses;
+        for (int i = 0; i < V; i++) {
+            adjList.add(new ArrayList<>());
+        }
+        for (int[] neighbour : prerequisites) {
+            int u = neighbour[0];
+            int v = neighbour[1];
+            adjList.get(v).add(u);
+        }
+        int[] indegree = new int[V];
+        for (int u = 0; u < V; u++) {
+            for (int v : adjList.get(u)) {
+                indegree[v]++;
+            }
+        }
+        Queue<Integer> queue = new LinkedList<>();
+        for (int i = 0; i < V; i++) {
+            if (indegree[i] == 0) {
+                queue.add(i);
+            }
+        }
+        ArrayList<Integer> result = new ArrayList<>();
+        while (!queue.isEmpty()) {
+            int node = queue.poll();
+            result.add(node);
+            for (int v : adjList.get(node)) {
+                indegree[v]--;
+                if (indegree[v] == 0) {
+                    queue.offer(v);
+                }
+            }
+        }
+        int[] res = new int[V];
+        for(int i=0; i<V; i++){
+            res[i] = result.get(i);
+        }
+        return res;
+    }
+}
