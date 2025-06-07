0b88d0afc50f5fcb9c15bef95a654f3ad746d070
Anand Pandey <91401035+anandpandey01@users.noreply.github.com>
2025-06-07 07:36:46 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Shortest path in Directed Acyclic Graph/shortest-path-in-directed-acyclic-graph.java
+++ Difficulty: Medium/Shortest path in Directed Acyclic Graph/shortest-path-in-directed-acyclic-graph.java
@@ -1,38 +1,46 @@
 // User function Template for Java
 class Solution {
-
     public int[] shortestPath(int V, int E, int[][] edges) {
         // Code here
         ArrayList<ArrayList<int[]>> adjList = new ArrayList<>();
-        for(int i=0; i<V; i++){
+        for (int i = 0; i < V; i++) {
             adjList.add(new ArrayList<>());
         }
-        for(int[] edge : edges){
-            adjList.get(edge[0]).add(new int[]{edge[1],edge[2]});
+        for (int[] edge : edges) {
+            adjList.get(edge[0]).add(new int[] {edge[1], edge[2]});
         }
-        int[] dist = new int[V];
-        Arrays.fill(dist,Integer.MAX_VALUE);
-        dist[0]=0;
-        Queue<int[]> queue = new LinkedList<>();
-        queue.add(new int[]{0,0});
-        while(!queue.isEmpty()){
-            int[] node = queue.poll();
-            int u =  node[0];
-            int d = node[1];
-            for(int[] neighbour : adjList.get(u)){
-                int v = neighbour[0];
-                int wt = neighbour[1];
-                if(dist[v] > d + wt ) {
-                    dist[v] = d + wt;
-                    queue.offer(new int[]{v, dist[v]});
-                }
+        int[] indegree = new int[V];
+        for (int u = 0; u < V; u++) {
+            for (int[] v : adjList.get(u)) {
+                indegree[v[0]]++;
             }
         }
-        
-        for(int i = 0; i < V; i++) {
-        if(dist[i] == Integer.MAX_VALUE) dist[i] = -1;
+        Queue<Integer> queue = new LinkedList<>();
+        for (int i = 0; i < V; i++) {
+            if (indegree[i] == 0)
+                queue.offer(i);
+        }
+        int[] dist = new int[V];
+        Arrays.fill(dist, Integer.MAX_VALUE);
+        dist[0] = 0;
+
+        while (!queue.isEmpty()) {
+            int u = queue.poll();
+            for (int[] neighbour : adjList.get(u)) {
+                int v = neighbour[0];
+                int wt = neighbour[1];
+                if (dist[u] != Integer.MAX_VALUE && dist[v] > dist[u] + wt) {
+                    dist[v] = dist[u] + wt;
+                }
+                indegree[v]--;
+                if (indegree[v] == 0)  queue.offer(v);
+            }
+        }
+
+        for (int i = 0; i < V; i++) {
+            if (dist[i] == Integer.MAX_VALUE)
+                dist[i] = -1;
         }
         return dist;
-        
     }
 }
