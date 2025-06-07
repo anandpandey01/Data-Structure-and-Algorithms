9081f037bbbdd6447a0214e7c3c61258cb10008e
Anand Pandey <91401035+anandpandey01@users.noreply.github.com>
2025-06-07 17:11:45 +0530

    Time: 28 ms (22.27%), Space: 46 MB (23.64%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.java
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.java
@@ -1,34 +1,43 @@
-class Solution {
+class Solution { //Dijkstra's
+    // Can be solved by changing PQ to Queue because all distances are 1 its a unweighted graph
     public int shortestPathBinaryMatrix(int[][] grid) {
         int rows = grid.length;
         int cols = grid[0].length;
-        if (grid[0][0] != 0 || grid[rows - 1][cols - 1] != 0) return -1;
+        if (grid[0][0] != 0 || grid[rows - 1][cols - 1] != 0)
+            return -1;
 
-        int[][] directions = {{-1,0},{1,0},{0,1},{0,-1},{1,1},{1,-1},{-1,1},{-1,-1}};
+        int[][] directions = { { -1, 0 }, { 1, 0 }, { 0, 1 }, { 0, -1 }, { 1, 1 }, { 1, -1 }, { -1, 1 }, { -1, -1 } };
         int V = grid.length;
-        Queue<int[]> queue = new LinkedList<>();
-        queue.offer(new int[]{0,0});
+        int[][] dist = new int[rows][cols];
+        for (int[] row : dist)
+            Arrays.fill(row, Integer.MAX_VALUE);
+
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> Integer.compare(a[2], b[2]));
+        pq.offer(new int[] { 0, 0, 0 });
         grid[0][0] = 1;
-        int count=0;
-        while(!queue.isEmpty()){
-            count++;
-            int size = queue.size();           
-            while(size-- >0){
-                int[] node= queue.poll();
-                int row = node[0];
-                int col = node[1];
-                if (row == rows - 1 && col == cols - 1) return count;
-                for(int[] neighbours : directions){
-                    int nr = row+ neighbours[0];
-                    int nc = col+ neighbours[1];
-                    if(nr<0 || nr>=rows || nc<0 || nc>=cols) continue;
-                    if(grid[nr][nc]==0){
+        dist[0][0] = 0;
+
+        while (!pq.isEmpty()) {
+            int[] node = pq.poll();
+            int row = node[0];
+            int col = node[1];
+            int dis = node[2];
+            for (int[] neighbours : directions) {
+                int nr = row + neighbours[0];
+                int nc = col + neighbours[1];
+                int wt = 1;
+                if (nr < 0 || nr >= rows || nc < 0 || nc >= cols)
+                    continue;
+                if (grid[nr][nc] == 0) {
+                    if (dist[nr][nc] > dis + wt) {
+                        dist[nr][nc] = dis+ wt;
                         grid[nr][nc] = 1;
-                        queue.offer(new int[]{nr,nc});
+                        pq.offer(new int[] { nr, nc, dis + wt });
                     }
                 }
             }
+
         }
-        return -1;
+        return dist[rows - 1][cols - 1] == Integer.MAX_VALUE ? -1 : dist[rows - 1][cols - 1] + 1;
     }
 }
