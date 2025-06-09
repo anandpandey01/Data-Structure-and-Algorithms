8b855256df7f67c073f4e44ade710761445f24c3
Anand Pandey <91401035+anandpandey01@users.noreply.github.com>
2025-06-09 11:17:25 +0530

    Time: 29 ms (38.78%), Space: 44.9 MB (72.24%) - LeetHub

--- 0947-most-stones-removed-with-same-row-or-column/0947-most-stones-removed-with-same-row-or-column.java
+++ 0947-most-stones-removed-with-same-row-or-column/0947-most-stones-removed-with-same-row-or-column.java
@@ -1,32 +1,36 @@
 class Solution {
     public int removeStones(int[][] stones) {
-        ArrayList<ArrayList<Integer>> adjList = new ArrayList<>();
-        int V = stones.length;
-        for(int i=0; i<V; i++)  adjList.add(new ArrayList<>());
-        for (int i = 0; i < V; i++) {
-            for (int j = i + 1; j < V; j++) {
-                if (stones[i][0] == stones[j][0] || stones[i][1] == stones[j][1]) {
-                adjList.get(i).add(j);
-                adjList.get(j).add(i);
+        int n = stones.length;
+        int[] parent = new int[n];
+        for(int i=0; i<n; i++) parent[i] = i;
+        int[] rank = new int[n];
+
+        int components=n;
+        for(int i=0; i<n; i++){
+            for(int j=i+1; j<n; j++){
+                if(stones[i][0] == stones[j][0] || stones[i][1] == stones[j][1]){
+                    int parentI = find(parent, i);
+                    int parentJ = find(parent, j);
+                    if (parentI != parentJ) {
+                        union(parent, rank, parentI, parentJ);
+                        components--;
+                    }
                 }
             }
         }
-        boolean[] visited = new boolean[V];
-        int components=0;
-        for(int i=0; i<V; i++){
-            if(visited[i]== false){
-                dfs(adjList,visited,i);
-                components++;
-            }
-        }
-        return V-components;
+        return n-components;
     }
-    public void dfs(ArrayList<ArrayList<Integer>> adjList, boolean[] visited, int u){
-        visited[u] = true;
-        for(int v:adjList.get(u)){
-            if(visited[v]== false){
-                dfs(adjList,visited,v);
-            }
+
+    public int find(int[] parent, int x){
+        if(parent[x] != x)  parent[x] = find(parent,parent[x]);
+        return parent[x];
+    }
+    public void union(int[] parent, int[]rank ,int x, int y){
+        if(rank[x]>rank[y]) parent[y] = x;
+        else if (rank[x]<rank[y]) parent[x] = y;
+        else{
+            parent[x] = y;
+            rank[y]++;
         }
     }
 }
