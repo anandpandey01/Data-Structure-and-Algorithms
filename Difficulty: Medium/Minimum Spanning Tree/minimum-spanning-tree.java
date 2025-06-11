a3f4602a8b15bbc45ce16e67405e22d27ffe0f77
Anand Pandey <91401035+anandpandey01@users.noreply.github.com>
2025-06-11 14:12:55 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Minimum Spanning Tree/minimum-spanning-tree.java
+++ Difficulty: Medium/Minimum Spanning Tree/minimum-spanning-tree.java
@@ -2,29 +2,21 @@ class Solution {
     static int spanningTree(int V, int E, List<List<int[]>> adj) {
         // Code Here.
         boolean[] visited = new boolean[V];
-        int[] parent = new int[V];
-        Arrays.fill(parent,-1);
-        int mstCost = 0;
-        
         PriorityQueue<int[]> pq = new PriorityQueue<>((a,b) -> Integer.compare(a[2],b[2]));
-        pq.offer(new int[]{-1,0,0});
-        
+        pq.offer(new int[]{-1,0,0});  // parent current weight
+        int mst = 0;
         while(!pq.isEmpty()){
             int[] node = pq.poll();
-            int parentNode = node[0], curNode = node[1], weight=node[2];
-            
-            if (visited[curNode] == true) continue;
-            visited[curNode] = true;
-            mstCost += weight;
-            
-            for(int[] neighbour: adj.get(curNode)){
-                int adjNode = neighbour[0];
-                int edgeWeight = neighbour[1];
-                if (visited[adjNode] == false) {
-                    pq.offer(new int[]{curNode, adjNode, edgeWeight});
+            int parent = node[0]; int curr = node[1]; int wt = node[2];
+            if(visited[curr] == true) continue;
+            visited[curr] = true;
+            mst = mst+ wt;
+            for(int[] v: adj.get(curr)){
+                if(visited[v[0]]== false){
+                    pq.offer(new int[]{curr,v[0],v[1]});
                 }
             }
         }
-        return mstCost;
+        return mst;
     }
 }
