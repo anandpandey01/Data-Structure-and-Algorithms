cbf0c082e71eb851edf251fccc437269c27df577
Anand Pandey <91401035+anandpandey01@users.noreply.github.com>
2025-06-11 16:03:25 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Minimum Spanning Tree - Kruskal's Algorithm/minimum-spanning-tree-kruskals-algorithm.java
+++ Difficulty: Medium/Minimum Spanning Tree - Kruskal's Algorithm/minimum-spanning-tree-kruskals-algorithm.java
@@ -2,36 +2,43 @@
 class Solution {
     static int kruskalsMST(int V, int[][] edges) {
         // code here
-        Arrays.sort(edges , (a,b) -> Integer.compare(a[2],b[2]));
-        int[] parent = new int[V];
-        int[] rank =   new int[V];
-        for(int i=0; i<V; i++)  parent[i] = i;
+        int[] par = new int[V];
+        for(int i=0; i<V; i++) par[i] = i;
+        int[] rank = new int[V];
+        Arrays.sort(edges,(a,b) -> Integer.compare(a[2],b[2]));
         
-        int mst=0, totalEdge=0;
+        int mst=0,totaledges=0;
         for(int[] edge : edges){
-            int u = edge[0]; int v = edge[1]; int wt = edge[2];
-            int pu = find(parent,u);
-            int pv = find(parent,v);
+            int u =edge[0]; int v = edge[1]; int wt = edge[2];
             
-            if(pu!= pv){
-                mst = mst+wt;
-                union(parent,rank,pu,pv);
-                totalEdge++;
-                if(totalEdge == V-1) break;
+            int u_par = find(par,u); int v_par = find(par,v);
+            if(u_par != v_par){
+                mst= mst+wt;
+                union(par, rank,u_par,v_par);
+                totaledges++;
+                if(totaledges == V-1) break;
             }
         }
         return mst;
     }
-    
-    static int find(int[] parent, int x){
-        if(parent[x] != x)  parent[x] = find(parent,parent[x]);
-        return parent[x];
-    } 
-    static void union(int[] parent,int[] rank, int x, int y){
-        if(rank[x]>rank[y]) parent[y] = x;
-        else if(rank[y]>rank[x]) parent[x] = y;
+    static int find(int[] par, int x){
+        if(par[x]!=x) par[x] = find(par, par[x]);
+        return par[x];
+    }
+    static void union(int[] par, int[] rank, int x, int y){
+        int x_par = find(par, x);
+        int y_par = find(par,y);
+        if(x_par == y_par) return;
+        
+        if(rank[x_par]>rank[y_par]){
+            par[y_par] = x_par;
+        }
+        else if(rank[x_par]<rank[y_par]){
+            par[x_par] = y_par;
+        }
         else{
-            parent[x] = y;   rank[y]++;
+            par[x_par] = y_par;
+            rank[y_par]++;
         }
     }
 }
