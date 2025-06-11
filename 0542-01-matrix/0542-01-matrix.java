29f6df452a96c99ce045bdada830ee76f7455f58
Anand Pandey <91401035+anandpandey01@users.noreply.github.com>
2025-06-11 13:24:33 +0530

    Time: 12 ms (92.87%), Space: 47.3 MB (65.23%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.java
+++ 0542-01-matrix/0542-01-matrix.java
@@ -1,37 +1,36 @@
-class Solution {   // Use a BFS and will traverse from 0 not 1
+class Solution { // Doing BFS from 0 to 1 because its much more optimised than 1 to 0
     int rows;
     int cols;
+
     public int[][] updateMatrix(int[][] mat) {
         rows = mat.length;
         cols = mat[0].length;
-        int [][] dist = new int[rows][cols];
+        int[][] dist = new int[rows][cols];
         Queue<int[]> queue = new LinkedList<>();
-        for(int i=0; i<rows; i++){
-            for(int j=0; j<cols; j++){
-                if(mat[i][j]==0){
-                    dist[i][j]=0;
-                    queue.offer(new int[]{i,j});
-                }
-                else{
-                    dist[i][j]=-1;
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (mat[i][j] == 0) {
+                    dist[i][j] = 0;
+                    queue.offer(new int[] { i, j });
+                } else {
+                    dist[i][j] = -1;
                 }
             }
         }
-        int adjList[][] = {{1,0},{-1,0},{0,1},{0,-1}};
+        int[][]adjList = {{-1,0},{0,1},{1,0},{0,-1}};
         while(!queue.isEmpty()){
-            int[] cell = queue.poll();
+            int []cell = queue.poll();
             int row = cell[0];
             int col = cell[1];
-            for(int[] neigbour : adjList){
-                int r = neigbour[0]+row;
-                int c = neigbour[1]+col;
-                if(r<0 || r>=rows || c<0 || c>=cols) continue;
-                if(dist[r][c]!=-1) continue;
-                dist[r][c] = dist[row][col]+1;
-                queue.offer(new int[]{r,c});               
+            for(int[] dir : adjList){
+                int nr = row+dir[0]; int nc = col+dir[1];
+                if(nr>=0 && nr<rows && nc>=0 && nc<cols && dist[nr][nc]==-1){
+                    dist[nr][nc] = dist[row][col]+1;
+                    queue.offer(new int[]{nr,nc});
+                }
+
             }
         }
         return dist;
-
     }
 }
