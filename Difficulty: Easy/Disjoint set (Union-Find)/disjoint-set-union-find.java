5436cd7d582f2dd0f3acaeafc63e05f6f385903a
Anand Pandey <91401035+anandpandey01@users.noreply.github.com>
2025-06-11 15:11:01 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Disjoint set (Union-Find)/disjoint-set-union-find.java
+++ Difficulty: Easy/Disjoint set (Union-Find)/disjoint-set-union-find.java
@@ -1,20 +1,28 @@
 /*Complete the function below*/
 class GfG {
     int find(int par[], int x) {
-      // add code here
-    if (par[x] == x){
-        return x;
+        // add code here.
+        if(par[x]!= x)  par[x] = find(par,par[x]);
+        return par[x];
     }
-    return find(par, par[x]);
-}
 
-void unionSet(int par[], int x, int z) {
-    // add code here.
-    
-    int x_parent = find(par, par[x]);
-    int z_parent = find(par, par[z]);
-    if (x_parent != z_parent){
-        par[x_parent] = z_parent;
+    void unionSet(int par[], int x, int z) {
+        // add code here.
+        
+        int[] rank = new int[par.length];
+        
+        int x_par = find(par,x);  int z_par = find(par,z);
+        if(x_par == z_par) return;
+        
+        if(rank[x_par]> rank[z_par]){
+            par[z_par] = x_par;
+        }
+        else if(rank[x_par]< rank[z_par]){
+            par[x_par] = z_par;
+        }
+        else{
+            par[x_par] = z_par;
+            rank[z_par]++;
+        }
     }
-}
 }
