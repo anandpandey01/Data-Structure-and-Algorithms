72348d09bc160799d46ad53a8941a6a6ba6c07e0
Anand Pandey <91401035+anandpandey01@users.noreply.github.com>
2025-06-12 07:33:01 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Dijkstra Algorithm/dijkstra-algorithm.java
+++ Difficulty: Medium/Dijkstra Algorithm/dijkstra-algorithm.java
@@ -1,33 +1,30 @@
 class Solution {
     public int[] dijkstra(int V, int[][] edges, int src) {
         // code here
+        // Storing Parent is optional you may chose not to.
         ArrayList<ArrayList<int[]>> adjList = new ArrayList<>();
-        for(int i=0; i<V; i++){
-            adjList.add(new ArrayList<>());
-        }
+        for(int i=0; i<V; i++) adjList.add(new ArrayList<>());
         for(int[] edge: edges){
             adjList.get(edge[0]).add(new int[]{edge[1],edge[2]});
-            adjList.get(edge[1]).add(new int[]{edge[0],edge[2]});
         }
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b) -> Integer.compare(a[1],b[1]));
-        int []dist = new int[V];
+        PriorityQueue<int[]> queue = new PriorityQueue<>((a,b)-> Integer.compare(a[2],b[2]));
+        queue.offer(new int[]{-1,src,0});
+        int[] dist = new int[V];
         Arrays.fill(dist, Integer.MAX_VALUE);
-        pq.offer(new int[]{src,0});
         dist[src] = 0;
-        while(!pq.isEmpty()){
-            int []node = pq.poll();
-            int value = node[0];
-            int d = node[1];
-            for(int[] neighbour: adjList.get(value)){
-                int v  = neighbour[0];
-                int wt = neighbour[1];
-                if(dist[v]>dist[value]+wt){
-                    dist[v]= dist[value]+wt;
-                    pq.offer(new int[]{v,dist[v]});
+        
+        while(!queue.isEmpty()){
+            int[] node= queue.poll();
+            int parent = node[0]; int u= node[1]; 
+            for(int[] neighbour:adjList.get(u)){
+                int v= neighbour[0]; int wt= neighbour[1];
+                if(dist[u]!= Integer.MAX_VALUE && (dist[v]> dist[u]+ wt)){
+                    dist[v]= dist[u]+ wt;
+                    queue.offer(new int[]{u,v,dist[v]});
                 }
-       
             }
         }
         return dist;
+        
     }
 }
