a888e35b196263d92eba3372f8fcd2b616b15f91
Anand Pandey <91401035+anandpandey01@users.noreply.github.com>
2025-06-12 15:57:32 +0530

    Time: 5 ms (97.54%), Space: 44.2 MB (83.53%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.java
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.java
@@ -1,40 +1,23 @@
-class Solution {
+class Solution {    // Bellman Ford
     public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
-        int rows = flights.length;
-        int cols = flights[0].length;
-        Map<Integer, List<int[]>> adjList = new HashMap<>();
-        for(int i=0; i<n; i++){
-            adjList.put(i,new ArrayList<>());
-        }
-        for(int[] flight : flights){
-            adjList.get(flight[0]).add(new int[]{flight[1],flight[2]});
-        }
+        int[] dist = new int[n];
+        Arrays.fill(dist,Integer.MAX_VALUE);
+        dist[src]=0;
 
-        int []dist = new int[n];
-        Arrays.fill(dist, Integer.MAX_VALUE);
-        dist[src] = 0;
-
-        Queue<int[]> queue = new LinkedList<>();
-        queue.offer(new int[]{src,0});
-        int steps =0;
-        while(!queue.isEmpty() && steps<=k){
-            int size = queue.size();
-            for (int i = 0; i < size; i++) {
-                int[] node= queue.poll();
-                int u = node[0];
-                int len = node[1];
-                for(int[] neighbor : adjList.get(u)){
-                    int v = neighbor[0], price = neighbor[1];
-                    if(dist[v]>price+len){
-                        dist[v] = price+len;
-                        queue.offer(new int[]{v,dist[v]});
-                    }
-                    
+        for(int i=0; i<=k; i++){                   // k stops  k+1 edges
+            int[] temp = Arrays.copyOf(dist, n); 
+            for(int[]flight : flights){
+                int u=flight[0];  int v=flight[1];  int wt= flight[2];
+                if(dist[u]!=Integer.MAX_VALUE && temp[v]>dist[u]+wt){
+                    temp[v] = dist[u]+wt;
                 }
             }
-            steps++;
+            dist = temp;
         }
-        return (dist[dst]== Integer.MAX_VALUE ? -1 : dist[dst]);
- 
+        return (dist[dst]== Integer.MAX_VALUE)?-1:dist[dst];
     }
 }
+
+
+//If you update dist[v] and then use that updated value in the same iteration, you risk including paths with more
+// than k + 1 edges. 
