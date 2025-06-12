db0b9b378907932879562b7a556c0f880c20ebf3
Anand Pandey <91401035+anandpandey01@users.noreply.github.com>
2025-06-12 15:14:29 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Floyd Warshall/floyd-warshall.java
+++ Difficulty: Medium/Floyd Warshall/floyd-warshall.java
@@ -3,16 +3,17 @@
 class Solution {
     public void floydWarshall(int[][] dist) {
         // Code here
-        int n = dist.length;
+        int n= dist.length;    // All vertices
+        
         for(int k=0; k<n; k++){
             for(int i=0; i<n; i++){
                 for(int j=0; j<n; j++){
-                     if (dist[i][k] != 1e8 && dist[k][j] != 1e8) {
+                    if(dist[i][k]!= 1e8 && dist[k][j]!= 1e8){
                         dist[i][j] = Math.min(dist[i][j], dist[i][k]+dist[k][j]);
                     }
                 }
-            }  
+            }
         }
-        
+        // If diagonal elements are -ve there is a -ve cycle
     }
 }
