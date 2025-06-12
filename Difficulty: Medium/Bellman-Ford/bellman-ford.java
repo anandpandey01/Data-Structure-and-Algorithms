d3c74f85ad067b74d74dd9f72d5aa443f0836cf0
Anand Pandey <91401035+anandpandey01@users.noreply.github.com>
2025-06-12 14:57:37 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Bellman-Ford/bellman-ford.java
+++ Difficulty: Medium/Bellman-Ford/bellman-ford.java
@@ -4,31 +4,25 @@ class Solution {
     public int[] bellmanFord(int V, int[][] edges, int src) {
         // code here
         int[] dist = new int[V];
-        Arrays.fill(dist, (int)1e8);
-        dist[src]=0;
+        Arrays.fill(dist,(int)1e8);
+        dist[src] = 0;
         
         for(int i=0; i<V-1; i++){
             for(int[] edge : edges){
-                int u =  edge[0];
-                int v =  edge[1];
-                int wt = edge[2];
-                if(dist[u] != (int)1e8){
-                    if(dist[v]>dist[u]+wt ){
-                       dist[v]=dist[u]+wt;
-                    }
+                int u= edge[0];  int v= edge[1];  int wt= edge[2];
+                if(dist[u]!= 1e8 && dist[v]> dist[u]+wt){
+                    dist[v]= dist[u]+wt;
                 }
             }
         }
         for(int[] edge : edges){
-                int u =  edge[0];
-                int v =  edge[1];
-                int wt = edge[2];
-                if(dist[u] != (int)1e8){
-                    if(dist[v]>dist[u]+wt ){
-                       return new int[]{-1}; // Negative cycle detected
-                    }
-                }
-            }
+           int u= edge[0];  int v= edge[1];  int wt= edge[2];
+                if(dist[u]!= 1e8 && dist[v]> dist[u]+wt){
+                    return new int[]{-1};
+                }     
+        }
         return dist;
+        
+        
     }
 }
