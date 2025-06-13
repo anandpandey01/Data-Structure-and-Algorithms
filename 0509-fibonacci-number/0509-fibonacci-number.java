59d55aa80116e673279d3d0c6c951a78701a54aa
Anand Pandey <91401035+anandpandey01@users.noreply.github.com>
2025-06-13 13:17:50 +0530

    Time: 0 ms (100%), Space: 40.6 MB (39.41%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,13 +1,12 @@
-class Solution {                   // Bottom Up Approach
+class Solution {                        // Space Complexity 0
     public int fib(int n) {
         if(n<=1) return n;
-        int[] dp = new int[n+1];
-        Arrays.fill(dp,-1);
-        dp[0]=0; dp[1]=1;
+        int prev=0, curr=1;
         for(int i=2; i<=n; i++){
-            dp[i]= dp[i-1]+dp[i-2];
+            int next = prev+curr;
+            prev = curr;
+            curr = next;
         }
-        return dp[n];
+        return curr;
     }
-    
 }
