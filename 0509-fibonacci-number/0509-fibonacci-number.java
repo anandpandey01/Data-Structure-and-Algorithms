6ce62d42f35c77884b6b39459e37532aaafe8d56
Anand Pandey <91401035+anandpandey01@users.noreply.github.com>
2025-06-13 13:15:09 +0530

    Time: 0 ms (100%), Space: 40.3 MB (80.18%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,12 +1,13 @@
-class Solution {
+class Solution {                   // Bottom Up Approach
     public int fib(int n) {
+        if(n<=1) return n;
         int[] dp = new int[n+1];
         Arrays.fill(dp,-1);
-        return solve(n,dp);
+        dp[0]=0; dp[1]=1;
+        for(int i=2; i<=n; i++){
+            dp[i]= dp[i-1]+dp[i-2];
+        }
+        return dp[n];
     }
-    public int solve(int n, int[]dp){
-        if(n <=1) return n;
-        if(dp[n]!= -1) return dp[n];
-        return solve(n-1,dp)+ solve(n-2,dp);
-    }
+    
 }
