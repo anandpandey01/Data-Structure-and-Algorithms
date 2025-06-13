a1606152611d7515a99fc5ced92ff9c9d04578ce
Anand Pandey <91401035+anandpandey01@users.noreply.github.com>
2025-06-13 15:58:58 +0530

    Time: 0 ms (100%), Space: 40.7 MB (98.06%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -2,13 +2,17 @@ class Solution {                    // Recursion+ Memoization
     public int rob(int[] nums) {
         int n= nums.length;
         int[] dp = new int[n+1];
+        Arrays.fill(dp,-1);
         return solve(nums,0,n,dp);
     }
     public int solve(int[] nums,int i, int n,int[] dp){
         if(i >= nums.length) return 0;
+        if(dp[i]!= -1) return dp[i];
         int skip = solve(nums,i+1,n,dp);
         int steal = nums[i]+solve(nums,i+2,n,dp);
         dp[i]= Math.max(skip,steal);
         return dp[i];
     }
 }
+// The maximum amount of money that can be robbed from the first i + 1 houses 
+// (i.e. from house 0 to house i)
