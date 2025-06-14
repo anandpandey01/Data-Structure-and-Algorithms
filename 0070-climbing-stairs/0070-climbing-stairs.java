ff629e37e4270dc57be0dd30c5dccd77f755ef6a
Anand Pandey <91401035+anandpandey01@users.noreply.github.com>
2025-06-14 08:58:03 +0530

    Time: 0 ms (100%), Space: 40.4 MB (51.12%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,11 +1,17 @@
-class Solution {                        // Bottom Up
+class Solution {                         // Recursion+Memoization
     public int climbStairs(int n) {
-        if(n==1 || n==2) return n;
-        int[]dp = new int[n+1];
-        dp[0]=0; dp[1]=1; dp[2]=2; 
-        for(int i=3; i<=n; i++){
-            dp[i] = dp[i-1]+dp[i-2];
-        }
+       int[] dp = new int[46];    //  int[] dp = new int[n+1];
+       Arrays.fill(dp,-1);
+       return solve(n,dp);
+    }
+    public int solve(int n, int[]dp){
+        if(n<0) return 0;
+        if(n==0) return 1;
+        if(dp[n]!= -1) return dp[n];
+        int one_step = solve(n-1,dp);
+        int two_step = solve(n-2,dp);
+        dp[n]= one_step+two_step;
         return dp[n];
     }
 }
+// dp[n] stores no of distinct ways to go to n stairs
