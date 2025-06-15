6e744d49099efe143b5cb804c98a85625cdcd0f5
Anand Pandey <91401035+anandpandey01@users.noreply.github.com>
2025-06-15 16:16:49 +0530

    Time: 51 ms (48.57%), Space: 44.3 MB (77.89%) - LeetHub

--- 1048-longest-string-chain/1048-longest-string-chain.java
+++ 1048-longest-string-chain/1048-longest-string-chain.java
@@ -1,22 +1,19 @@
 class Solution {
     public int longestStrChain(String[] words) {
-        int n= words.length;
-        int[][]dp = new int[n+1][n+1];
-        for(int i=0; i<=n; i++) Arrays.fill(dp[i],-1);
+        int n = words.length;
+        int[]dp = new int[n+1];
+        Arrays.fill(dp,1);
         Arrays.sort(words, (a,b) -> Integer.compare(a.length(), b.length()));
-        return solve(words,dp,n,-1,0);
-
-    }
-    public int solve(String[] words,int[][] dp, int n,int prevIndex,int i){
-        if(i >=n ) return 0;
-        if(dp[prevIndex+1][i]!= -1) return dp[prevIndex+1][i];
-        int take = 0;
-        if(prevIndex== -1 || isPredecessor(words[prevIndex],words[i])){
-            take = 1+solve(words,dp,n,i,i+1);
+        int maxLIS=1;
+        for(int i=0; i<n; i++){
+            for(int j=0; j<i; j++){
+                if(isPredecessor(words[j],words[i])){
+                    dp[i] = Math.max(dp[i],dp[j]+1);
+                    maxLIS = Math.max(maxLIS,dp[i]); 
+                }
+            }
         }
-        int skip = solve(words,dp,n,prevIndex,i+1);
-        dp[prevIndex+1][i] = Math.max(take,skip);
-        return dp[prevIndex+1][i];
+        return maxLIS;
     }
     public boolean isPredecessor(String prevWord, String currWord){
         int m = prevWord.length();  int n = currWord.length();
