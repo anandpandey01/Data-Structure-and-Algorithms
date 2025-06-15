c78d797455f1a704b2d7b4e10e8a844c288338a1
Anand Pandey <91401035+anandpandey01@users.noreply.github.com>
2025-06-15 14:26:35 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Longest Increasing Subsequence/longest-increasing-subsequence.java
+++ Difficulty: Medium/Longest Increasing Subsequence/longest-increasing-subsequence.java
@@ -1,23 +1,20 @@
 class Solution {
     static int lis(int arr[]) {
         // code here
-        int n= arr.length;
+        int n = arr.length;
         int[][]dp = new int[n+1][n+1];
-        for(int i=0; i<=n; i++) Arrays.fill(dp[i],-1);
+        for(int i=0; i<=n; i++) Arrays.fill(dp[i], -1);
         return solve(arr,dp,n,0,-1);
     }
-    public static int solve(int[]arr, int[][]dp,int n,int i,int prevIndex){
+    static int solve(int[]arr, int[][]dp, int n, int i, int prevIndex){
         if(i>=n) return 0;
-        if(dp[prevIndex+1][i]!= -1) return dp[prevIndex+1][i];
-
+        if(dp[i][prevIndex+1]!= -1) return dp[i][prevIndex+1];
         int take=0;
         if(prevIndex == -1 || arr[i]>arr[prevIndex]){
-            take = 1+solve(arr,dp,n,i+1,i);
+            take = 1 + solve(arr,dp,n,i+1,i);
         }
-        int skip = solve(arr,dp,n,i+1,prevIndex);    // Note
-        dp[prevIndex+1][i] = Math.max(take,skip);
-
-        return dp[prevIndex+1][i];
+        int skip = solve(arr,dp,n,i+1,prevIndex);
+        dp[i][prevIndex+1] = Math.max(take,skip);
+        return dp[i][prevIndex+1];
     }
-    
 }
