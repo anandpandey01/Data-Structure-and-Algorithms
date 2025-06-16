742df3766caa1a4e877259f2aa8cc95a9fe248fe
Anand Pandey <91401035+anandpandey01@users.noreply.github.com>
2025-06-16 16:24:25 +0530

    Time: 152 ms (5.74%), Space: 55.3 MB (7.03%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.java
+++ 0072-edit-distance/0072-edit-distance.java
@@ -3,20 +3,20 @@ class Solution {
         int m=word1.length();  int n= word2.length();
         int[][]dp = new int[501][501];
         for(int i=0; i<501; i++) Arrays.fill(dp[i],-1);
-        return solve(word1,word2,dp,m,n,0,0);
+        return solve(word1,word2,dp,m,n);
     }
-    public int solve(String word1, String word2,int[][]dp,int m, int n,int i, int j){
-        if(i>=m) return n-j;   // Addition
-        if(j>=n) return m-i;   //Deletion
-        if(dp[i][j]!= -1) return dp[i][j];
-        if(word1.charAt(i) == word2.charAt(j)) dp[i][j] = solve(word1,word2,dp,m,n,i+1,j+1);
+    public int solve(String word1, String word2,int[][]dp,int m, int n){
+        if(m==0) return n;   
+        if(n==0) return m;  
+        if(dp[m][n]!= -1) return dp[m][n];
+        if(word1.charAt(m-1) == word2.charAt(n-1)) dp[m][n] = solve(word1,word2,dp,m-1,n-1);
         else{
-            int insert =  1+solve(word1,word2,dp,m,n,i,j+1);
-            int delete =  1+solve(word1,word2,dp,m,n,i+1,j);
-            int replace = 1+solve(word1,word2,dp,m,n,i+1,j+1);  
-            dp[i][j] = Math.min(insert,Math.min(delete,replace));
+            int insert =  1+solve(word1,word2,dp,m,n-1);
+            int delete =  1+solve(word1,word2,dp,m-1,n);
+            int replace = 1+solve(word1,word2,dp,m-1,n-1);  
+            dp[m][n] = Math.min(insert,Math.min(delete,replace));
         }
-        return dp[i][j];
+        return dp[m][n];
 
     }
 }
