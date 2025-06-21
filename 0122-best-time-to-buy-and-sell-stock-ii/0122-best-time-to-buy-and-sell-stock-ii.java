b510459486bc2addb336135cb27c703af72d3c5e
Anand Pandey <91401035+anandpandey01@users.noreply.github.com>
2025-06-21 08:57:40 +0530

    Time: 3 ms (11.64%), Space: 48.5 MB (7.73%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
@@ -5,23 +5,23 @@ class Solution {
         n = prices.length;
         dp = new int[n][2];
         for(int i=0; i<n; i++) Arrays.fill(dp[i],-1);
-        return solve(prices,0,true);
+        return solve(prices,0,1);                       // flag 1 - Buy, 0 - Sell
     }
-    public int solve(int[] prices, int i, boolean flag){    // flag 1 - Buy, 0 - Sell
+    public int solve(int[] prices, int i, int flag){    
         if(i==n) return 0;
-        int f = (flag== true)?1:0;
-        if(dp[i][f]!= -1) return dp[i][f];
+
+        if(dp[i][flag]!= -1) return dp[i][flag];
         int profit=0;
-        if(flag == true){
-            int buy =    -prices[i] + solve(prices, i+1, !flag);
-            int notBuy =  solve(prices, i+1, flag);
+        if(flag == 1){
+            int buy =    -prices[i] + solve(prices, i+1, 0);
+            int notBuy =  solve(prices, i+1, 1);
             profit = Math.max(buy,notBuy);
         }
         else{
-            int sell =   prices[i]+ solve(prices, i+1, !flag);
-            int notSell = solve(prices,i+1, flag);
+            int sell =   prices[i]+ solve(prices, i+1, 1);
+            int notSell = solve(prices,i+1, 0);
             profit = Math.max(sell,notSell);
         }
-        return dp[i][f] = profit;
+        return dp[i][flag] = profit;
     }
 }
