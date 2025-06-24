5fc7c9d9273f2a247545f83adc985f0a0da79b14
Anand Pandey <91401035+anandpandey01@users.noreply.github.com>
2025-06-24 07:50:07 +0530

    Time: 15 ms (78.39%), Space: 44.3 MB (81.19%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -1,25 +1,16 @@
 class Solution {
-    int n;
-    int[][] dp;
     public int coinChange(int[] coins, int amount) {
-        n = coins.length;  dp = new int[n + 1][amount + 1];
+        int[] dp = new int[amount + 1];
+        Arrays.fill(dp, amount + 1);
+        dp[0] = 0;
 
-        for (int i = 0; i <= n; i++)  dp[i][0] = 0;
-        for (int j = 1; j <= amount; j++)  dp[0][j] = Integer.MAX_VALUE;
-
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= amount; j++) {
-                int include = Integer.MAX_VALUE;
-                if (coins[i - 1] <= j) {
-                    int res = + dp[i][j - coins[i - 1]];
-                    if (res != Integer.MAX_VALUE) {
-                        include = 1 + res;
-                    }
+        for (int i = 1; i <= amount; i++) {
+            for (int coin : coins) {
+                if (coin <= i) {
+                    dp[i] = Math.min(dp[i], dp[i - coin] + 1);
                 }
-                int exclude = dp[i - 1][j];
-                dp[i][j] = Math.min(include, exclude);
             }
         }
-        return dp[n][amount] >= Integer.MAX_VALUE ? -1 : dp[n][amount];
+        return dp[amount] > amount ? -1 : dp[amount];
     }
 }
