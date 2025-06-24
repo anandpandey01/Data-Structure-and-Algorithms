91652494dad26886fd1dd26b36fca106d48b2a82
Anand Pandey <91401035+anandpandey01@users.noreply.github.com>
2025-06-24 11:57:50 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Knapsack with Duplicate Items/knapsack-with-duplicate-items.java
@@ -0,0 +1,24 @@
+// User function Template for Java
+
+class Solution {
+    static int n;
+    static int[][]dp;
+    static int knapSack(int val[], int wt[], int W) {
+        // code here
+        n = wt.length;
+        dp = new int[n+1][W+1];
+        for(int i=0; i<=n; i++) Arrays.fill(dp[i],-1);
+        return solve(W,val,wt,0);
+        
+    }
+    static int solve(int W, int val[], int wt[], int i){
+        if(i==n) return 0;
+        if(dp[i][W]!= -1) return dp[i][W];
+        int take =0;
+        if(wt[i]<=W){
+            take = val[i]+ solve(W-wt[i],val,wt,i);
+        }
+        int skip = solve(W,val,wt,i+1);
+        return dp[i][W] =  Math.max(take,skip);
+    }
+}
