62d4efc7d490d7375d946380219f9b80f9ec1b2d
Anand Pandey <91401035+anandpandey01@users.noreply.github.com>
2025-06-27 07:51:40 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Fractional Knapsack/fractional-knapsack.java
+++ Difficulty: Medium/Fractional Knapsack/fractional-knapsack.java
@@ -1,30 +1,32 @@
 class Solution {
     class Items{
-        int value,weight;
+        int value;
+        int weight;
         Items(int value,int weight){
             this.value = value;
             this.weight = weight;
         }
     }
     double fractionalKnapsack(int[] values, int[] weights, int W) {
-        // code here
-        int n = values.length;
-        Items[] arr = new Items[n];
+        int n = weights.length;
+        Items[] item = new Items[n];
         for(int i=0; i<n; i++){
-            arr[i] = new Items(values[i], weights[i]);
+            item[i] = new Items(values[i],weights[i]);
         }
-        double maxVal = 0;
-        Arrays.sort(arr, (a,b)-> Double.compare((double)b.value/b.weight ,(double)a.value/a.weight));
+        Arrays.sort(item, (a,b)-> Double.compare((double)b.value/b.weight,(double)a.value/a.weight));
+        // Note the casting must be done before division
+        
+        double result=0;
         for(int i=0; i<n; i++){
-            if(W>=arr[i].weight){
-                W-=arr[i].weight;
-                maxVal+=arr[i].value;
+            if(W>=item[i].weight){
+                W-=item[i].weight;
+                result+=item[i].value;  
             }
             else{
-                maxVal+= W *((double) arr[i].value / arr[i].weight);  //Note the brackets
-                break;
+                result+=W*((double)item[i].value/item[i].weight); break;
+                //Note the brackets
             }
         }
-        return maxVal;
+        return result;
     }
 }
