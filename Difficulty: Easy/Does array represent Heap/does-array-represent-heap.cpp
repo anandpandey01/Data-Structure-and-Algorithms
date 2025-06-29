1e3c934a2072a0d71ec81bde61e42a38d85a4c36
Anand Pandey <91401035+anandpandey01@users.noreply.github.com>
2025-06-29 18:18:27 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Does array represent Heap/does-array-represent-heap.cpp
@@ -0,0 +1,14 @@
+
+class Solution {
+  public:
+    bool isMaxHeap(int arr[], int n) {
+        // Your code goes here
+        for(int i=0; i<n; i++){
+            int left =  (2*i)+1;
+            int right = (2*i)+2;
+            if(left<n &&  arr[left]> arr[i]) return false;
+            if(right<n && arr[right]>arr[i]) return false;
+        }
+        return true;
+    }
+};
