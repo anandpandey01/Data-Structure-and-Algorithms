87b200b54e95cc2aae6f1491205716737a669271
Anand Pandey <91401035+anandpandey01@users.noreply.github.com>
2025-07-01 15:17:09 +0530

    Time: 0 ms (100%), Space: 41.4 MB (24.38%) - LeetHub

--- 0980-unique-paths-iii/0980-unique-paths-iii.java
+++ 0980-unique-paths-iii/0980-unique-paths-iii.java
@@ -1,42 +1,38 @@
 class Solution {
-    int row;
-    int col;
-    int totalCount; 
-    int result;
-    int[][] directions = {{-1,0},{1,0},{0,1},{0,-1}};
+    int rows; int cols; int nonObstacleCount; int totalPath;
+    int[][] directions = {{1,0},{-1,0},{0,1},{0,-1}};
     public int uniquePathsIII(int[][] grid) {
-        row = grid.length; col = grid[0].length; 
-        totalCount=0; result=0;
-        int startX = 0, startY = 0;                
-        for(int i=0; i<row; i++){
-            for(int j=0; j<col; j++){
-                if(grid[i][j] == 0){
-                    totalCount++;  
-                }
+        rows = grid.length;
+        cols = grid[0].length;
+        nonObstacleCount = 0;  totalPath = 0;
+        int start_x = 0; int start_y =0;
+        for(int i=0; i<rows; i++){
+            for(int j=0; j<cols; j++){
                 if(grid[i][j] == 1){
-                    startX = i;
-                    startY = j;
-                    totalCount++;  // For 1 Starting Point that is also a nonobstacle
+                    start_x = i; start_y= j;
+                    nonObstacleCount++;
+                }
+                else if(grid[i][j] == 0){
+                    nonObstacleCount++;
                 }
             }
         }
-        dfs(grid, startX, startY, 0);  //start with count 1 for the start cell
-        return result;
+        solve(grid,start_x,start_y,0);
+        return totalPath;
     }
-    public void dfs(int[][] grid, int i, int j,int nonobstacleCount){
-        if(i<0 || i>=row || j<0 || j>=col) return ;
-        if(grid[i][j] == -1) return ;
-        if(grid[i][j] == 2) {
-            if (nonobstacleCount == totalCount) result++;
+    public void solve(int[][] grid, int i, int j, int count){
+        if(i<0 || i>=rows || j<0 || j>=cols) return;
+        if(grid[i][j] == -1) return;
+        if(grid[i][j] == 2){
+            if(count == nonObstacleCount) totalPath++;
             return;
         }
         int temp = grid[i][j];
         grid[i][j] = -1;
-        for(int[]dir: directions){
-            int nr = dir[0]+i;   int nc = dir[1]+j;
-            dfs(grid,nr,nc,nonobstacleCount+1);
+        for(int[] dir: directions){
+            int nr = dir[0]+i;  int nc = dir[1]+j;
+            solve(grid,nr,nc,count+1);
         }
-        grid[i][j] = temp;    //BackTrack
+        grid[i][j] = temp;
     }
-
 }
