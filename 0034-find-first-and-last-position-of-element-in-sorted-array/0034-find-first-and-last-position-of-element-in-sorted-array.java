ea4b49e7f651629b229bd4a9ee68403306f344af
Anand Pandey <91401035+anandpandey01@users.noreply.github.com>
2025-07-07 07:27:16 +0530

    Time: 0 ms (100%), Space: 45.8 MB (93.52%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,33 +1,34 @@
 class Solution {
-    public int[] searchRange(int[] arr, int x) {
-        int first=-1,last=-1;
-        int low=0, high=arr.length-1;
+    public int[] searchRange(int[] arr, int target) {
+        int n = arr.length;
+        int low = 0, high = n-1;
+        int first = -1;
         while(low<=high){
-            int mid=(low+high)/2;
-            if(arr[mid]==x){
-               first=mid;
-               high=mid-1;
-              
+            int mid = low+(high-low)/2;
+            if(arr[mid]>target){
+                high = mid-1;
             }
-            else if(arr[mid]>x){
-                high=mid-1;
+            else if(arr[mid]<target){
+                low = mid+1;
             }
-            else if(arr[mid]<x){
-                low=mid+1;
+            else{
+                first = mid;
+                high = mid-1;
             }
         }
-        low=0; high=arr.length-1;
+        low = 0; high = n-1;
+        int last = -1;
         while(low<=high){
-            int mid=(low+high)/2;
-            if(arr[mid]==x){
-                 last=mid;
-                 low=mid+1;
+            int mid = low+(high-low)/2;
+            if(arr[mid]>target){
+                high = mid-1;
             }
-            else if(arr[mid]>x){
-                high=mid-1;
+            else if(arr[mid]<target){
+                low = mid+1;
             }
-            else if(arr[mid]<x){
-                low=mid+1;
+            else{
+                last = mid;
+                low = mid+1;
             }
         }
         return new int[]{first,last};
