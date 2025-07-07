84d576016436afd57e404e2ad9bcdc228eddf88a
Anand Pandey <91401035+anandpandey01@users.noreply.github.com>
2025-07-07 15:14:18 +0530

    Time: 4 ms (98.9%), Space: 60 MB (22.74%) - LeetHub

--- 2439-minimize-maximum-of-array/2439-minimize-maximum-of-array.java
+++ 2439-minimize-maximum-of-array/2439-minimize-maximum-of-array.java
@@ -1,32 +1,15 @@
 class Solution {
-    public int minimizeArrayValue(int[] arr) {
-        int n = arr.length; 
-        int max = arr[0];  int ans = 0;
-        for(int i=1; i<n; i++){
-            max = Math.max(max,arr[i]);
-        }
-        int low=1, high = max;
-        while(low<=high){
-            int mid = low+(high-low)/2;
-            if(isValid(arr,mid)){
-                ans = mid;
-                high = mid-1;
-            }
-            else{
-                low = mid+1;
-            }
-        }
-        return ans;
+    public int minimizeArrayValue(int[] nums) {
+        int n = nums.length;
+        long sum = 0;
+        int ans = 0;
+    
+    for (int i = 0; i < n; i++) {
+        sum += nums[i];
+        int avg = (int)((sum + i) / (i + 1));   // ceiling of average
+        ans = Math.max(ans, avg);
     }
-    public boolean isValid(int[] arr, int minPossible){
-        int m = arr.length;
-        long[] nums = new long[m];
-        for (int i = 0; i < m; i++) nums[i] = arr[i];
-        for(int i=0; i<m-1; i++){
-            if(nums[i]> minPossible) return false;
-            long buffer = minPossible - nums[i];
-            nums[i+1] = nums[i+1]- buffer;
-        }
-        return (nums[m-1]<=minPossible);
+    
+    return ans;
     }
 }
