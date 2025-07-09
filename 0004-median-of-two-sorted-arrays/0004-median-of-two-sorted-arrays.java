62592c05c73a6d4bcb643d19a1ffd36c3f420daa
Anand Pandey <91401035+anandpandey01@users.noreply.github.com>
2025-07-09 16:06:20 +0530

    Time: 1 ms (100%), Space: 45.8 MB (97.31%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
@@ -1,37 +1,35 @@
-class Solution {   //Brute Force
+class Solution {  //Binary Search
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        int m = nums1.length,  n = nums2.length;
-        int[] result = new int[m+n];
-        int i=0, j=0;
-        int idx = 0;
-        while(i<m && j<n){
-            if(nums1[i]<nums2[j]){
-                result[idx++] = nums1[i];
-                i++;         
-            }
-            else{
-                result[idx++] = nums2[j];
-                j++;
-            }
+        if(nums1.length > nums2.length){
+            return findMedianSortedArrays(nums2,nums1);     // Use Shorter Array for Optimization
         }
-        while(i<m){
-            result[idx++] = nums1[i]; 
-            i++;
+        int m = nums1.length;   int n = nums2.length;
+
+        int low = 0, high = m;    //Note
+        while(low<=high){
+            int Px = low+(high-low)/2;    // Total Elements in Left Half from 1st Array
+            int Py = (m+n+1)/2 - Px;      // (m+n+1)/2 is total elements if divide in 2 parts Odd or Even
+
+            int x1 = (Px==0)?Integer.MIN_VALUE: nums1[Px-1]; 
+            int x2 = (Py==0)?Integer.MIN_VALUE:  nums2[Py-1];
+            int x3 = (Px == m)?Integer.MAX_VALUE:nums1[Px];
+            int x4 = (Py == n)?Integer.MAX_VALUE:nums2[Py];
+
+            if(x4>=x1 && x3>=x2){
+                if((m+n) %2 == 0){
+                    return (Math.max(x1,x2) + Math.min(x3,x4)) / 2.0;
+                }
+                else{
+                    return Math.max(x1,x2);
+                }
+            }
+            else if(x4<x1){
+                high = Px-1;    
+            }
+            else if(x3<x2){
+                low = Px+1;
+            }
         }  
-        while(j<n){
-            result[idx++] = nums2[j];
-            j++;
-        }
-        double ans = 0;
-        int end = m+n;
-        int mid = (0+end)/2;
-        if(end%2 == 0){
-            ans =  (result[mid-1]+result[mid])/2.0;     // Double
-        }
-        else{
-            ans = result[mid];
-        }
-        return ans;
-        
-    }  
+        return -1;  
+    }
 }
