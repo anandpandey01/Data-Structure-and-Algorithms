82b2f5df80024e3560f7d22a4fd577d5e615bb75
Anand Pandey <91401035+anandpandey01@users.noreply.github.com>
2025-07-17 07:48:40 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Smallest subarray with sum greater than x/smallest-subarray-with-sum-greater-than-x.java
+++ Difficulty: Easy/Smallest subarray with sum greater than x/smallest-subarray-with-sum-greater-than-x.java
@@ -6,18 +6,18 @@ class Solution {
         // Your code goes here
         int n = arr.length;
         int i=0, j=0;
+        int prefixSum = 0, result = Integer.MAX_VALUE;
         
-        int minLength = Integer.MAX_VALUE;
-        int sum = 0;
         while(j < n){
-            sum+=arr[j];
-            while(sum > x){
-               minLength = Math.min(minLength, j-i+1);
-               sum-=arr[i];
-               i++;
+            prefixSum += arr[j];
+            while(prefixSum > x){
+                result = Math.min(result, j-i+1);
+                prefixSum -= arr[i];
+                i++;
             }
             j++;
         }
-        return (minLength == Integer.MAX_VALUE)?0 : minLength;
+        return (result == Integer.MAX_VALUE)?0 : result;
+        
     }
 }
