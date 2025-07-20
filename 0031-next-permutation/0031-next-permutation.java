37c7d3b45d76cc1e38c486db075e9e784b477e5b
Anand Pandey <91401035+anandpandey01@users.noreply.github.com>
2025-07-20 20:08:52 +0530

    Time: 0 ms (100%), Space: 42.9 MB (95.09%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.java
+++ 0031-next-permutation/0031-next-permutation.java
@@ -1,20 +1,37 @@
 class Solution {
-    public void nextPermutation(int[] arr) {
-        int n=arr.length;
-        int i=n-1;
-        while(i>0 && arr[i-1]>=arr[i]){
-            i--;    
-        }
-        if(i>0){
-            int j=n-1;
-             while (arr[j] <= arr[i - 1]) {
-                j--;
+    public void nextPermutation(int[] nums) {
+        int n = nums.length;
+        int first_greater = -1;
+        // Traverse from right to left and find the first element where arr[i-1] < arr[i] 
+        for(int i=n-1; i>0; i--){
+            if(nums[i] > nums[i-1]){
+                first_greater = i-1;   
+                break;
             }
-            int temp = arr[j];
-             arr[j]= arr[i-1];
-             arr[i-1]= temp;
         }
-        Arrays.sort(arr,i,n);
-
+        // Find the number to Swap With     
+        if(first_greater != -1){
+            int swap_index = -1;
+            for(int j= n-1; j>= first_greater; j--){
+            if(nums[j] > nums[first_greater]){
+                swap_index = j;
+                break;
+                }   
+            }
+            if(swap_index != -1){
+                int temp = nums[swap_index];
+                nums[swap_index] = nums[first_greater];
+                nums[first_greater] = temp;
+            }
+        }
+            
+        // Reverse the array from first_greater+1
+        int i = first_greater+1, j = n-1;
+        while(i < j){
+            int temp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = temp;
+            i++; j--;
+        }
     }
 }
