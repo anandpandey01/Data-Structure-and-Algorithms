3994f83b90ace42b68db39038458936f6787df81
Anand Pandey <91401035+anandpandey01@users.noreply.github.com>
2025-07-25 17:01:46 +0530

    Time: 0 ms (100%), Space: 42.1 MB (18.31%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.java
+++ 0063-unique-paths-ii/0063-unique-paths-ii.java
@@ -1,26 +1,21 @@
 class Solution {
-    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+    // Instead of Previous Logic you can simply think as soon as you encounter 1
+    // in 1st row or col leave them as they are by default 0
+    // Much easier than previous Approach
+    
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {      
         int m = obstacleGrid.length;  int n = obstacleGrid[0].length;
-        int[][]dp = new int[m+1][n+1];
-        if (obstacleGrid[0][0] == 1) return 0;
-        dp[0][0]= 1;
-        for(int col=1; col<n; col++){
-            if(obstacleGrid[0][col] == 1) dp[0][col] = 0;
-            else if(obstacleGrid[0][col-1] == 1){
-                dp[0][col]=0;
-                obstacleGrid[0][col] = 1;
-            } 
-            else dp[0][col] = 1;
-        } 
-        for(int row=1; row<m; row++){
-            if(obstacleGrid[row][0] == 1) dp[row][0] = 0;
-            else if(obstacleGrid[row-1][0] == 1){
-                dp[row][0]=0;
-                obstacleGrid[row][0] = 1;
-            } 
-            else dp[row][0] = 1;
-        } 
-
+        if(obstacleGrid[m-1][n-1] == 1) return 0;
+        int[][] dp = new int[m][n];
+        for (int i = 0; i < n; i++) {
+            if (obstacleGrid[0][i] == 1) break;
+            dp[0][i] = 1;
+        }
+        for (int i = 0; i < m; i++) {
+            if (obstacleGrid[i][0] == 1) break;
+            dp[i][0] = 1;
+        }
+        
         for(int i=1; i<m;i++){
             for(int j=1; j<n; j++){
                 if(obstacleGrid[i][j] == 1) dp[i][j] = 0;
@@ -30,5 +25,7 @@ class Solution {
             }
         }
         return dp[m-1][n-1];
+        
     }
+    
 }
