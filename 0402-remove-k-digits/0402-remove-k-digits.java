a6faa694c87a279078d17c589eaa35b134598642
Anand Pandey <91401035+anandpandey01@users.noreply.github.com>
2025-07-31 08:25:37 +0530

    Time: 11 ms (91.61%), Space: 44.9 MB (80.62%) - LeetHub

--- 0402-remove-k-digits/0402-remove-k-digits.java
+++ 0402-remove-k-digits/0402-remove-k-digits.java
@@ -1,29 +1,28 @@
-class Solution {  // Monotonic Stack
-    public String removeKdigits(String num, int k) {
-        if(num.length() == k) return "0";
-        
-        Stack<Integer> st = new Stack<>();
-        for(char ch : num.toCharArray()){
-            int value = ch - '0';
-            while(!st.isEmpty() && st.peek() > value && k > 0){
+class Solution {
+    public String removeKdigits(String str, int k) {
+        if(str.length()==k) return "0";
+        Stack<Character>st = new Stack<>();
+        for(char ch : str.toCharArray()){
+            while(!st.isEmpty() && k>0 && st.peek()>ch){
                 st.pop();
                 k--;
             }
-            st.push(value);
+            st.push(ch);
         }
-        while(k > 0){
-            st.pop(); k--;
+        while(k>0){
+            st.pop();
+            k--;
         }
         StringBuilder sb = new StringBuilder();
-        for(int val : st){
-            sb.append(val);                  // Not popping so no need to reverse
+        for(char ch : st){
+            sb.append(ch);    // Not popping so no need to reverse
         }
-        int d = 0;
-        String result = "";
-        while(d < sb.length() && sb.charAt(d) == '0'){
+        int d=0;
+        while( d<sb.length() && sb.charAt(d)=='0'){
             d++;
         }
-        result = sb.substring(d);
-        return (result.isEmpty()) ? "0": result;
+        String res = sb.substring(d);
+        return (res.isEmpty())? "0": res;
+
     }
 }
