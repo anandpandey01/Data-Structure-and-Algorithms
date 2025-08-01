6809f107e20d153bd02fec891f9e42165e856efb
Anand Pandey <91401035+anandpandey01@users.noreply.github.com>
2025-08-01 14:24:12 +0530

    Time: 161 ms (25.23%), Space: 46.4 MB (64.03%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.java
+++ 0127-word-ladder/0127-word-ladder.java
@@ -1,35 +1,38 @@
 class Solution {
     public int ladderLength(String beginWord, String endWord, List<String> wordList) {
-        HashSet<String> set = new HashSet<>(wordList);
+        HashSet<String> set = new HashSet<>();
+        for(int i=0; i<wordList.size(); i++){
+            set.add(wordList.get(i));
+        }
+        if(!set.contains(endWord)) return 0;
         Queue<String> queue = new LinkedList<>();
         queue.offer(beginWord);
         if(set.contains(beginWord)) set.remove(beginWord);
 
-        int level=0;
+        int count = 0;
         while(!queue.isEmpty()){
-            int size= queue.size();
-            while(size-- >0){
-                String node = queue.poll();
-                if(node.equals(endWord)) return level+1;
-                List<String> neighbours = getNeighbours(node,set);
-                for(String word : neighbours){    
-                    queue.offer(word);
-                    set.remove(word);
+            int size = queue.size();
+            while(size-- > 0){
+                String node  = queue.poll();
+                if(node.equals(endWord)) return count+1;
+                for(String neighbour : getNeighbour(node, set)){
+                    queue.offer(neighbour);
+                    set.remove(neighbour);
                 }
             }
-            level++;
+            count++;
         }
         return 0;
     }
-    public List<String> getNeighbours(String word, HashSet<String> set){
-        List<String> neighbour = new ArrayList<>();
+    public List<String> getNeighbour(String word, HashSet<String> set){
+        List<String> list = new ArrayList<>();
         for(int i=0; i<word.length(); i++){
-            for(char ch='a'; ch<='z';ch++){
-                if(ch == word.charAt(i)) continue;
-                String newWord = word.substring(0,i)+ch+word.substring(i+1,word.length());
-                if(set.contains(newWord)) neighbour.add(newWord);
+            for(char ch = 'a'; ch <= 'z'; ch++){
+                if(word.charAt(i) == ch) continue;
+                String newWord = word.substring(0,i) + ch + word.substring(i+1);
+                if(set.contains(newWord)) list.add(newWord);
             }
         }
-        return neighbour;
+        return list;
     }
 }
