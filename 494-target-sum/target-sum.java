864110964259c4b7f98621b46f5b7a4ba657cf2d
Anand Pandey <91401035+anandpandey01@users.noreply.github.com>
2025-08-06 15:10:38 +0530

    Time: 4 ms (91.74%) | Memory: 44.5 MB (35.30%) - LeetSync

--- 494-target-sum/target-sum.java
+++ 494-target-sum/target-sum.java
@@ -1,23 +1,28 @@
 class Solution {
     int n;
-    //Integer[][] dp;
+    Integer[][]dp;
     public int findTargetSumWays(int[] nums, int target) {
         n = nums.length;
-        //int maxSum = 0;    for(int i=0; i<n; i++) maxSum += nums[i];
-       // dp = new Integer[n][maxSum+1];
-        return solve(nums, target, 0, 0);
+        int sum = 0;
+        for(int i=0; i<n; i++) sum += nums[i];
+        // s1 - s2 = target , s1 + s2 = sum 
+        int s1 = (sum + target)/2;     // Subsets Sum equals k
+        if ((sum + target) % 2 != 0 || (sum + target < 0)) return 0;   // Note Imp
+
+        dp = new Integer[n][s1+1]; 
+        return solve(nums, s1, 0);
+
     }
-
-    public int solve(int[] nums, int target, int sum, int i){
-        if(i == n && target == sum) return 1;
-        if(i == n ) return 0;
-        //if(dp[i][sum] != null) return dp[i][sum];
-        int positive = solve(nums, target, sum + nums[i], i+1);
-        int negative = solve(nums, target, sum - nums[i], i+1);
-
-        
-        //return dp[i][sum] = positive + negative;
-        return positive + negative;
+    public int solve(int[] nums, int sum, int i){
+        if(i == n && sum == 0) return 1;
+        if(i == n) return 0;
+        if(dp[i][sum] != null) return dp[i][sum];
+        int take = 0;
+        if(nums[i] <= sum){
+            take = solve(nums, sum - nums[i], i+1);
+        } 
+        int skip = solve(nums, sum , i+1);
+        return dp[i][sum] =  take + skip;
 
     }
 }
