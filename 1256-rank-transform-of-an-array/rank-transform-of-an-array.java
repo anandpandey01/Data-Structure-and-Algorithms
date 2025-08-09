b00df92025238279ac955c95e941a115033fc199
Anand Pandey <91401035+anandpandey01@users.noreply.github.com>
2025-08-09 14:54:35 +0530

    Time: 23 ms (94.23%) | Memory: 62.3 MB (62.65%) - LeetSync

--- 1256-rank-transform-of-an-array/rank-transform-of-an-array.java
+++ 1256-rank-transform-of-an-array/rank-transform-of-an-array.java
@@ -1,30 +1,21 @@
 class Solution {
-    class Pair{
-        int val;
-        int idx;
-        Pair(int val, int idx){
-            this.val = val;
-            this.idx = idx;
-        }
-    }
     public int[] arrayRankTransform(int[] arr) {
         int n = arr.length;
-        int[] result = new int[n];
-        PriorityQueue<Pair> pq = new PriorityQueue<>((a,b)-> Integer.compare(a.val,b.val));
+        int[] copy = new int[n];
+        for(int i=0; i<n; i++) copy[i] = arr[i];
+        
+        Arrays.sort(arr);
+        int j = 1;
+        HashMap<Integer, Integer> hs = new HashMap<>();
         for(int i=0; i<n; i++){
-            pq.offer(new Pair(arr[i],i));
-        }
-        int rank = 0, prev = Integer.MAX_VALUE;
-        while(!pq.isEmpty()){
-            Pair node = pq.poll();
-            int value = node.val, index = node.idx;
-            if(value != prev){
-                rank++;
-                prev = value;
+            if(!hs.containsKey(arr[i])){
+                hs.put(arr[i],j++);
             }
-            result[index] = rank;
+            
         }
-        return result;
-
+        for(int i=0; i<n; i++){
+            copy[i] = hs.get(copy[i]);
+        }
+        return copy;
     }
 }
