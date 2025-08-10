f8ad46642e7b563500b6a9e77ba3066a6892e940
Anand Pandey <91401035+anandpandey01@users.noreply.github.com>
2025-08-10 12:58:50 +0530

    Time: 1 ms (33.79%) | Memory: 41.9 MB (14.36%) - LeetSync

--- 678-valid-parenthesis-string/valid-parenthesis-string.java
+++ 678-valid-parenthesis-string/valid-parenthesis-string.java
@@ -1,30 +1,35 @@
 class Solution {
     public boolean checkValidString(String s) {
-    int n = s.length();
-    boolean[][] dp = new boolean[n+1][n+1];
+        Stack<Integer> openSt = new Stack<>();
+        Stack<Integer> asterisksSt = new Stack<>();
 
-    dp[n][0] = true; // Base case: at the end of string, only count == 0 is valid
-    for (int i = n - 1; i >= 0; i--) {
-        for (int count = 0; count <= n; count++) {
+        for (int i = 0; i < s.length(); i++) {
             char ch = s.charAt(i);
+
             if (ch == '(') {
-                if (count + 1 <= n) {
-                    dp[i][count] = dp[i + 1][count + 1];
+                openSt.push(i);
+            } else if (ch == '*') {
+                asterisksSt.push(i);
+            } else {
+                if (!openSt.isEmpty()) {
+                    openSt.pop();
+                } else if (!asterisksSt.isEmpty()) {
+                    asterisksSt.pop();
+                } else {
+                    return false;
                 }
-            } 
-            else if (ch == ')') {
-                if (count - 1 >= 0) {
-                    dp[i][count] = dp[i + 1][count - 1];
-                }
-            } 
-            else {
-                boolean case1 = (count+1 <= n) && dp[i+1][count+1]; 
-                boolean case2 = dp[i+1][count];                        
-                boolean case3 = (count - 1 >= 0) && dp[i+1][count-1]; 
-                dp[i][count] = case1 || case2 || case3;
             }
         }
-    }
-    return dp[0][0];
+
+        // This post processing will be required for cases like - "*(())(*"
+        while (!openSt.isEmpty() && !asterisksSt.isEmpty()) {
+            if (openSt.peek() > asterisksSt.peek()) {
+                return false;
+            }
+            openSt.pop();
+            asterisksSt.pop();
+        }
+
+        return openSt.isEmpty();
     }
 }
